20cda16e03015f7e0d03fb3be4a523ec0498b390
张诏 <2321885538@qq.com>
2022-01-02 11:27:54 +0800

    feat: added PanelGroup component and count-to dependence

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="dashboard-container">
     <div class="dashboard-text">name: {{ name }}</div>
+    <PanelGroup/>
   </div>
 </template>
 
 <script>
+import PanelGroup from './components/PanelGroup'
 import { mapGetters } from 'vuex'
 
 export default {
   name: 'Dashboard',
+  components: {
+    PanelGroup
+  },
   computed: {
     ...mapGetters([
       'name'
