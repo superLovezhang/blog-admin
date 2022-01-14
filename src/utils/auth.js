1e8cd3faf92d3eaa38b4cb1ba973dcd7ac264daa
张诏 <2321885538@qq.com>
2022-01-14 19:31:30 +0800

    modified the global VUE_APP_BASE_API variable and request.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,13 @@
-import Cookies from 'js-cookie'
-
-const TokenKey = 'vue_admin_template_token'
+const TokenKey = 'blog_admin_token_key'
 
 export function getToken() {
-  return Cookies.get(TokenKey)
+  return window.localStorage.get(TokenKey)
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  return window.localStorage.set(TokenKey, token)
 }
 
 export function removeToken() {
-  return Cookies.remove(TokenKey)
+  return window.localStorage.remove(TokenKey)
 }
