f4406a7126c9d6d36e2fcb8a06cab24f8a054780
张诏 <2321885538@qq.com>
2022-02-21 09:09:34 +0800

    removed all files related mock

--- vue.config.js
+++ vue.config.js
@@ -29,16 +29,6 @@ module.exports = {
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
-  devServer: {
-    port: port,
-    open: true,
-    proxy: 'http://localhost:8000',
-    overlay: {
-      warnings: false,
-      errors: true
-    },
-    before: require('./mock/mock-server.js')
-  },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
     // it can be accessed in index.html to inject the correct title.
